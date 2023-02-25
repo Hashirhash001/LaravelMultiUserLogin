4ca60e4025782dd00e5d4488a249c391d41a4f2c
Hashir mv <hashmvhashmuhammed007@gmail.com>
2023-02-25 23:42:40 +0530

    initial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,9 +49,9 @@
                 <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
                     
 
-                    <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+                    <!-- <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
-                    </div>
+                    </div> -->
                 </div>
             </div>
         </div>
